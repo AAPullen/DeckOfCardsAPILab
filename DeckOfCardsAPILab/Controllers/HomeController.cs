5ac100bfb23dcb95179450b1baee01787c8f617b
Andrew <andrewpullen1@gmail.com>
2023-04-19 18:57:58 -0400

    init commit

---
+++ DeckOfCardsAPILab/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DeckOfCardsAPILab.Models;
+using DeckOfCardsAPILab.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCardsAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DeckApiService _deckApiService;
+        public HomeController(DeckApiService deckApiService)
+        {
+            _deckApiService = deckApiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                DrawnDeck.Deck result = await _deckApiService.GetDrawnCards();
+                return View(result);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Exception = ex;
+                Debug.WriteLine(ex);
+                return View();
+            }
+        }
+
+    }
+}
---
+++ DeckOfCardsAPILab/Services/DeckApiService.cs
@@ -0,0 +1,34 @@
+﻿using DeckOfCardsAPILab.Models;
+using System.Reflection.Metadata.Ecma335;
+using static System.Net.WebRequestMethods;
+
+namespace DeckOfCardsAPILab.Services
+{
+    public class DeckApiService
+    {
+        private readonly HttpClient _httpClient;
+        public DeckApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+   
+        public async Task<DrawnDeck.Deck> GetDrawnCards()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"https://deckofcardsapi.com/api/deck/new/draw/?count=5");
+            
+            DrawnDeck.Deck result = await response.Content.ReadAsAsync<DrawnDeck.Deck>();
+
+            return result;
+        }
+
+        public async Task<NewDeck> GetNewDeck()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("https://deckofcardsapi.com/api/deck/new/");
+
+            NewDeck result = await response.Content.ReadAsAsync<NewDeck>();
+
+            return result;
+        }
+
+    }
+}
